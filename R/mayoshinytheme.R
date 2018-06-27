eaa174f7dfdeeafe5eafb7b253609c4ec9c44803
Ethan Heinzen <heinzen.ethan@mayo.edu>
2018-06-27 17:16:47 -0500

    Initial commit for v1.0.0

---
+++ R/mayoshinytheme.R
@@ -0,0 +1,21 @@
+
+#' Use a Mayo Shiny Theme
+#'
+#' Style a Shiny app with Mayo colors.
+#'
+#' @details Use this as the \code{theme = } argument to the main shiny page-builder functions, e.g., \code{\link[shiny]{fluidPage}}.
+#' @return Returns a file path reference to the Mayo Shiny CSS file, and attaches that path so shiny knows about it.
+#' @seealso \code{\link[shinythemes]{shinytheme}}
+#' @examples
+#' \dontrun{
+#' shinyApp(
+#'   ui = fluidPage(theme = mayoshinytheme(), ...),
+#'   server = function(input, output) { }
+#' )
+#' }
+#' @export
+mayoshinytheme <- function()
+{
+  shiny::addResourcePath("mayoshinythemes", mayoshinypath())
+  file.path("mayoshinythemes", "www", "mayo_theme.css")
+}
---
+++ inst/rstudio/templates/project/resources/app.R
@@ -0,0 +1,55 @@
+
+library(shiny)
+
+ui <- navbarPage(
+  theme = mayoshiny::mayoshinytheme(),
+  "My cool new prediction app",
+  tabPanel(
+    "Navbar 1",
+    sidebarPanel(
+      fileInput("file", "File input:"),
+      textInput("txt", "Text input:", "general"),
+      sliderInput("slider", "Slider input:", 1, 100, 30),
+      tags$h5("Default actionButton:"),
+      actionButton("action", "Search"),
+
+      tags$h5("actionButton with CSS class:"),
+      actionButton("action2", "Action button", class = "btn-primary")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel(
+          "Tab 1",
+          h4("Table"),
+          tableOutput("table"),
+          h4("Verbatim text output"),
+          verbatimTextOutput("txtout"),
+          h1("Header 1"),
+          h2("Header 2"),
+          h3("Header 3"),
+          h4("Header 4"),
+          h5("Header 5")
+        ),
+        tabPanel("Tab 2", "This panel is intentionally left blank"),
+        tabPanel("Tab 3", "This panel is intentionally left blank")
+      )
+    )
+  ),
+  tabPanel("Navbar 2", "This panel is intentionally left blank"),
+  tabPanel("DISCLAIMER", mayoshiny::disclaimer())
+)
+
+
+server <- function(input, output) {
+  output$txtout <- renderText({
+    paste(input$txt, input$slider, format(input$date), sep = ", ")
+  })
+
+  output$table <- renderTable({
+    head(read.csv("data/testdata.csv"), 4)
+  })
+}
+
+
+# Run the application
+shinyApp(ui = ui, server = server)
