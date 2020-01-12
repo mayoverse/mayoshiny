73d9b1de30827048b6400f56539ca70b1cbb9319
overdodactyl <patjohns1216@gmail.com>
2020-01-12 01:14:43 -0500

    rename mayo_shiny to use_mayoshiny

--- R/mayoshiny.R
+++ R/mayoshiny.R
@@ -1,18 +1,8 @@
-
+#' \code{mayoshiny} package
+#'
 #' Templates for Shiny Apps at Mayo Clinic
-#'
-#' Templates for Shiny Apps at Mayo Clinic.
-#'
-#' @section Functions:
-#'
-#' \code{\link{mayo_shiny}}
-#'
-#' \code{\link{mayoshinytheme}}
-#'
-#' @examples
-#' library(mayoshiny)
 #'
 #' @docType package
 #' @name mayoshiny
-#'
+
 NULL
--- R/use_mayoshiny.R
+++ R/use_mayoshiny.R
@@ -5,17 +5,24 @@
 #' @param path A directory path for where to initialize the Shiny app.
 #' @param ... Other arguments (not in use at this time)
 #' @details Normally called when creating a new project in R Studio.
+#'    \code{mayoshiny} has been replaced by \code{use_mayoshiny} and will be removed in
+#'    future versions.
 #' @export
 mayo_shiny <- function(path, ...) {
+  if (as.character(match.call()[[1]]) == "mayo_shiny") {
+    warning(paste("please use use_mayoshiny() instead of mayo_shiny().",
+            "mayo_shiny() will be removed in future versions"), call. = FALSE)
+  }
   dir <- mayoshinypath()
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
   file.copy(file.path(dir, "app.R"), file.path(path, "app.R"))
   file.copy(file.path(dir, "DESCRIPTION"), file.path(path, "DESCRIPTION"))
   dir.create(file.path(path, "data"))
-  file.copy(
-    file.path(dir, "data", "testdata.csv"),
-    file.path(path, "data", "testdata.csv"))
 }
+
+#' @export
+#' @rdname mayo_shiny
+use_mayoshiny <- mayo_shiny
 
 
 mayoshinypath <- function() {
